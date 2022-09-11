9938798a52b48018cd8a718a60b72a254817fc56
SuperDork <bearbaier@gmail.com>
2022-09-11 11:55:49 -0500

    Added the files to the repo

---
+++ PocketMon Game/data/audio.js
@@ -0,0 +1,37 @@
+const audio = {
+    Map: new Howl({
+        src: '/Volumes/Coding/Coding Portfolio/PocketMon/PocketMon Game/Audio/map.wav',
+        html5: true,
+        volume: 0.1
+    }),
+    initBattle: new Howl({
+        src: '/Volumes/Coding/Coding Portfolio/PocketMon/PocketMon Game/Audio/initBattle.wav',
+        html5: true,
+        volume: 0.1
+    }),
+    battle: new Howl({
+        src: '/Volumes/Coding/Coding Portfolio/PocketMon/PocketMon Game/Audio/battle.mp3',
+        html5: true,
+        volume: 0.1
+    }),
+    tackleHit: new Howl({
+        src: '/Volumes/Coding/Coding Portfolio/PocketMon/PocketMon Game/Audio/tackleHit.wav',
+        html5: true,
+        volume: 0.1
+    }),
+    fireballHit: new Howl({
+        src: '/Volumes/Coding/Coding Portfolio/PocketMon/PocketMon Game/Audio/fireballHit.wav',
+        html5: true,
+        volume: 0.1
+    }),
+    initFireball: new Howl({
+        src: '/Volumes/Coding/Coding Portfolio/PocketMon/PocketMon Game/Audio/initFireball.wav',
+        html5: true,
+        volume: 0.1
+    }),
+    victory: new Howl({
+        src: '/Volumes/Coding/Coding Portfolio/PocketMon/PocketMon Game/Audio/victory.wav',
+        html5: true,
+        volume: 0.1
+    })
+}
